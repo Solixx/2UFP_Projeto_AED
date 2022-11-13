9fda6bf66a2bff7f3536f9ea18585c27b324899f
Manuel <manuel_jose_2002@hotmail.com>
2022-11-13 03:29:11 +0000

    Resuloção de problemas na Matrix Codificada
    Estourava o programa com mais de 3 valores no ficheiro
    add calloc em matrixCod

--- main.c
+++ main.c
@@ -19,16 +19,18 @@ void projetoAED(){
     char *fileName = "../data/chaves_publicas";
     FILE *fileChavesPubString = fopen(fileName, "r");
     int lines = 1, fileLinePos = 0;
-    char **matrixPub, **matrixPriv, matrixCod[lines][4];
+    char **matrixPub, **matrixPriv, **matrixCod;
 
     //Alocar espaço para a matriz e inicializar com 0
     matrixPub = calloc(lines * sizeof (char *), sizeof (char *));
     matrixPriv = calloc(lines * sizeof (char *), sizeof (char *));
+    matrixCod = calloc(lines * sizeof (char *), sizeof (char *));
 
     for (int i = 0; i < lines; ++i) {
         //Alocar espaço para cada nova linha da matriz e inicializar com 0
         matrixPub[i] = (char *) calloc(lines * sizeof (char *), sizeof (char));
         matrixPriv[i] = (char *) calloc(lines * sizeof (char *), sizeof (char));
+        matrixCod[i] = (char *) calloc(lines * sizeof (char *), sizeof (char));
         //Se já não haver valores para ler sai do array e para de alocar memoria
         if(fgets(matrixPub[lines-1], 100, fileChavesPubString) == NULL){
             break;
@@ -40,5 +42,25 @@ void projetoAED(){
         RL_V2_String(matrixPriv[i], matrixCod[i]);
         fileLinePos++;
         lines++;
+    }
+
+    printf("\tChaves Publicas\t\n");
+    for (int i = 0; i < lines-1; ++i) {
+        printf("%s\t", matrixPub[i]);
+    }
+    free(matrixPub);
+
+    printf("\n\tChaves Privadas\t\n");
+    for (int i = 0; i < lines-1; ++i) {
+        printf("%s\t", matrixPriv[i]);
+    }
+    free(matrixPriv);
+
+    printf("\n\tChaves Codificadas\t\n");
+    for (int i = 0; i < lines-1; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            printf("%c\t", matrixCod[i][j]);
+        }
+        printf("\n");
     }
 }
