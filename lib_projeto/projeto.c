57e5df498a2cc96f49a9aca79fc375fd2fc2b1b9
Manuel <84775761+Solixx@users.noreply.github.com>
2023-01-07 20:51:35 +0000

    Alguns comentarios

--- lib_projeto/projeto.c
+++ lib_projeto/projeto.c
@@ -6,69 +6,6 @@
 #include "aed1_lp1_2223_proj_part1_chars.h"
 #include "aed1_lp1_2223_proj_part1_ints.h"
 
-void main_lib_projeto(){
-
-    //cliente0();
-    //cliente1();
-    //cliente2();
-    //cliente3();
-}
-
-void cliente0(){
-    char t[] = "2211";
-
-    printf("%d\n", bipolar_number(t));
-}
-
-void cliente1(){
-    int k = 4;
-    int n[k];
-
-    //print_bipolar_numbers(k);
-    printf("%s\n", find_mul_bipolar_number("2014"));
-}
-
-void cliente2(){
-    char t[] = "77555";
-    int histo[10];
-
-    if(bipolar_number(t) == 0){
-        hist(t, histo);
-        for (int i = 0; i < 10; ++i) {
-            printf("histo de %d = %d\n", i, histo[i]);
-        }
-    }
-}
-
-void cliente3(){
-    char t[] = "77555";
-    char rL[4];
-    int r = 77555;
-    int runLess[4];
-    int mat[1][4];
-
-    if(bipolar_number(t) == 0){
-        for (int i = 0; i < 4; ++i) {
-            runLess[i] = 0;
-        }
-        //RL_V1(t, runLess);
-        //RL_V2(t, runLess);
-        RL_V2_Ints(r, mat[0]);
-        //RL_V2_String(t,rL);
-        printf("RL\n");
-        for (int i = 0; i < 4; ++i) {
-            //printf("%d ", runLess[i]);
-            //printf("%c ", rL[i]);
-        }
-        for (int i = 0; i < 1; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                printf("%d ", mat[i][j]);
-            }
-            printf("\n");
-        }
-    }
-}
-
 int bipolar_number(char *text){
 
     int bipolar = 0, changes = 0, exits = 0;
@@ -270,312 +207,6 @@ int find_mul_bipolar_number_Int(int k){
     }
 }
 
-int hist(char* num, int histo[]){
-    for (int i = 0; i < 10; ++i) {
-        histo[i] = 0;
-    }
-    for (int i = 0; i < strlen(num); ++i) {
-        histo[*(num+i)-'0']++;
-    }
-    return 0;
-}
-
-int RL_V1(char* num, int runLess[]){
-    int runLessPos = 0;
-    char temp;
-    for (int i = 0; i < strlen(num); ++i) {
-        if(i == 0){
-            runLess[runLessPos]++;
-        } else{
-            if(*(num+i) == temp){
-                runLess[runLessPos]++;
-            } else{
-                runLessPos++;
-                runLess[runLessPos] = temp-'0';
-                runLessPos++;
-                runLess[runLessPos]++;
-            }
-        }
-        temp = *(num+i);
-    }
-    runLessPos++;
-    runLess[runLessPos] = temp-'0';
-}
-
-int RL_V2(char *num, int runLess[]){
-    int runLessPos = 0;
-    runLess[runLessPos] = 1;
-    runLess[runLessPos+1] = *(num)-'0';
-    for (int i = 1; i < strlen(num); ++i) {
-        if(*(num+i) != *(num+(i-1))){
-            runLessPos += 2;
-            runLess[runLessPos+1] = *(num+i)-'0';
-        }
-        runLess[runLessPos]++;
-    }
-}
-
-int RL_V2_Ints(int num, int runLess[]){
-    int runLessPos = 0, digits;
-    digits = numDigits(num);
-    int allD[digits];
-    for (int i = 0; i < digits; ++i) {
-        runLess[i] = 0;
-    }
-    if(num != 0){
-        allDigits(num, allD);
-        runLess[runLessPos] = 1;
-        runLess[runLessPos+1] = allD[digits-1];
-        for (int i = digits-1; i >= 0; i--) {
-            if(allD[i] != allD[i-1]){
-                runLessPos += 2;
-                runLess[runLessPos+1] = allD[i-1];
-            }
-            runLess[runLessPos]++;
-        }
-    }
-    else{
-        return 0;
-    }
-}
-
-char* RL_V2_String(char* num, char rL[]){
-    int runLessPos = 0;
-    char count = '1';
-    *(rL+runLessPos) = count;
-    *(rL+(runLessPos+1)) = *(num);
-    for (int i = 1; i < strlen(num); ++i) {
-        if(*(num+i) != *(num+(i-1))){
-            runLessPos += 2;
-            *(rL+(runLessPos+1)) = *(num+i);
-            count='0';
-        }
-        count++;
-        *(rL+runLessPos) = count;
-    }
-}
-
-int countColumnPub(int digits, struct matrixString mString, int i, int columnsPub){
-    digits = numDigits(atoi(mString.matrixPub[i]));
-    if(digits > columnsPub){
-        columnsPub = digits;
-    }
-    return columnsPub;
-}
-int countColumnPriv(int digits, struct matrixString mString, int i, int columnsPriv){
-    digits = numDigits(atoi(mString.matrixPriv[i]));
-    if(digits > columnsPriv){
-        columnsPriv = digits;
-    }
-    return columnsPriv;
-}
-
-void printStringMatrixPub(struct matrixString mString, int lines){
-    printf("\tStrings - Chaves Publicas\t\n");
-    for (int i = 0; i < lines-1; ++i) {
-        printf("%s\t", mString.matrixPub[i]);
-    }
-}
-
-void printStringMatrixPriv(struct matrixString mString, int lines){
-    printf("\n\tStrings - Chaves Privadas\t\n");
-    for (int i = 0; i < lines-1; ++i) {
-        printf("%s\t", mString.matrixPriv[i]);
-    }
-}
-
-void printStringMatrixCod(struct matrixString mString, int lines){
-    printf("\n\tStrings - Chaves Codificadas\t\n");
-    for (int i = 0; i < lines-1; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            printf("%c\t", mString.matrixCod[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void printIntMatrixPub(struct matrixInts mInts, int lines, int columns){
-    printf("\n\tInts - Chaves Publicas\t\n");
-    for (int i = 0; i < lines-1; ++i) {
-        for (int j = 0; j < columns; ++j) {
-            if(mInts.matrixPub[i][j] >= 0) printf("%d\t", mInts.matrixPub[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void printIntMatrixPriv(struct matrixInts mInts, int lines, int columns){
-    printf("\n\tInts - Chaves Privadas\t\n");
-    for (int i = 0; i < lines-1; ++i) {
-        for (int j = 0; j < columns; ++j) {
-            if(mInts.matrixPriv[i][j] >= 0) printf("%d\t", mInts.matrixPriv[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void printIntMatrixCod(struct matrixInts mInts, int lines){
-    printf("\n\tInts - Chaves Codificadas\t\n");
-    for (int i = 0; i < lines-1; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            if(mInts.matrixCod[i][j] >= 0) printf("%d\t", mInts.matrixCod[i][j]);
-            else printf("0\t");
-        }
-        printf("\n");
-    }
-}
-
-int readFromFileString(struct matrixString mString, struct matrixInts mInts, int lines, FILE *fileChavesPubRead, char *fileName, int columns[], int digits, int pos){
-
-    if(lines < pos) printf("Insira uma posicao possivel\n");
-    fileChavesPubRead = fopen(fileName, "r");
-
-    for (int i = pos; i < lines; ++i) {
-
-        //Alocar espaço para cada nova linha da matriz e inicializar com 0
-        mString.matrixPub[i] = (char *) calloc(lines * sizeof (char*), sizeof (char));
-        mString.matrixPriv[i] = (char *) calloc(lines * sizeof (char*), sizeof (char));
-        mString.matrixCod[i] = (char *) calloc(lines * sizeof (char*), sizeof (char));
-
-        mInts.matrixPub[i] = (int *) calloc(lines * sizeof (int *), sizeof (int));
-        mInts.matrixPriv[i] = (int *) calloc(lines * sizeof (int *), sizeof (int));
-        mInts.matrixCod[i] = (int *) calloc(lines * sizeof (int *), sizeof (int));
-
-        //Se já não haver valores para ler sai do array e para de alocar memoria
-        if(fgets(mString.matrixPub[i], sizeof (mString.matrixPub[i]), fileChavesPubRead) == NULL){
-            free(mString.matrixPub[i]);
-            free(mString.matrixPriv[i]);
-            free(mString.matrixCod[i]);
-            free(mInts.matrixPub[i]);
-            free(mInts.matrixPriv[i]);
-            free(mInts.matrixCod[i]);
-            break;
-        }
-        mString = receiveFileMatrixString(mString, columns, digits, lines, pos);
-        lines++;
-    }
-    fclose(fileChavesPubRead);
-    return lines;
-}
-
-int readFromString(struct matrixString mString, struct matrixInts mInts, int lines, int columns[], int digits, int pos, char *key){
-
-    if(lines < pos) printf("Insira uma posicao possivel\n");
-
-        //Alocar espaço para cada nova linha da matriz e inicializar com 0
-        mString.matrixPub[pos] = (char *) calloc(lines * sizeof (char *), sizeof (char));
-        mString.matrixPriv[pos] = (char *) calloc(lines * sizeof (char *), sizeof (char));
-        mString.matrixCod[pos] = (char *) calloc(lines * sizeof (char *), sizeof (char));
-
-        mInts.matrixPub[pos] = (int *) calloc(lines * sizeof (int *), sizeof (int));
-        mInts.matrixPriv[pos] = (int *) calloc(lines * sizeof (int *), sizeof (int));
-        mInts.matrixCod[pos] = (int *) calloc(lines * sizeof (int *), sizeof (int));
-
-        //Se já não haver valores para ler sai do array e para de alocar memoria
-        mString.matrixPub[pos] = key;
-        mString = receiveMatrixString(mString, columns, digits, lines, pos);
-        lines++;
-    return lines;
-}
-
-struct matrixString receiveFileMatrixString(struct matrixString mString, int columns[], int digits, int lines, int pos){
-    for (int i = pos; i < lines; ++i) {
-        //Recebe os valores do ficheiro e retira o \n e conta o numero de colunas que vao ser necessárias para a matriz de inteiros (columnsPub)
-        mString.matrixPub[i] = strtok(mString.matrixPub[i], "\n");
-        *(mString.matrixPub[i]+ strlen(mString.matrixPub[i])) = '\0';
-        columns[0] = countColumnPub(digits, mString, i, columns[0]);
-
-        //Guarda o valor em matrixPriv[i]  e conta o numero de colunas que vao ser necessárias para a matriz de inteiros (columnsPriv)
-        if(strcmp(mString.matrixPub[i], "\0")){
-            mString.matrixPriv[i] = find_mul_bipolar_number(mString.matrixPub[i]);
-            columns[1] = countColumnPriv(digits, mString, i, columns[1]);
-        }
-
-        //Guarda o valor em matrixCod[i]
-        if(strcmp(mString.matrixPriv[i], "\0")){
-            RL_V2_String(mString.matrixPriv[i], mString.matrixCod[i]);
-        }
-    }
-    return mString;
-}
-struct matrixString receiveMatrixString(struct matrixString mString, int columns[], int digits, int lines, int pos){
-    //Recebe os valores do ficheiro e retira o \n e conta o numero de colunas que vao ser necessárias para a matriz de inteiros (columnsPub)
-    columns[0] = countColumnPub(digits, mString, pos, columns[0]);
-    //Guarda o valor em matrixPriv[i]  e conta o numero de colunas que vao ser necessárias para a matriz de inteiros (columnsPriv)
-    if(strcmp(mString.matrixPub[pos], "\0")){
-        mString.matrixPriv[pos] = find_mul_bipolar_number(mString.matrixPub[pos]);
-        columns[1] = countColumnPriv(digits, mString, pos, columns[1]);
-    }
-    //Guarda o valor em matrixCod[i]
-    if(strcmp(mString.matrixPriv[pos], "\0")){
-        RL_V2_String(mString.matrixPriv[pos], mString.matrixCod[pos]);
-    }
-    return mString;
-}
-
-void receiveMatrixPubInt(struct matrixString mString, struct matrixInts mInts, int columnsPub, int lines){
-    for (int i = 0; i < lines-1; ++i) {
-        //Array com digitos da mString.matrixPub[i]
-        int *allD = (int *) calloc(numDigits(atoi(mString.matrixPub[i])) * sizeof (*allD), sizeof (*allD));
-        allDigits(atoi(mString.matrixPub[i]), allD);
-        //Guardar os valores de allD em mInts.matrixPub[i][j-1]
-        for (int j = 1; j <= columnsPub; j++) {
-            //Se J ultrapassar o numero de digitos em mString.matrixPub[i] guarda o valor -1 (este vai ser usardo como valor de referencia para uma coluna vazia visto que é ncessário escrever os '0')
-            if(j <= numDigits(atoi(mString.matrixPub[i]))) {
-                mInts.matrixPub[i][j-1] = allD[numDigits(atoi(mString.matrixPub[i])) - j];
-            }else{
-                mInts.matrixPub[i][j-1] = -1;
-            }
-        }
-        free(allD);
-    }
-}
-
-void receiveMatrixPrivInt(struct matrixString mString, struct matrixInts mInts, int columnsPriv, int lines){
-    for (int i = 0; i < lines-1; ++i) {
-        //Array com digitos da mString.matrixPriv[i]
-        int *allD = (int *) calloc(numDigits(atoi(mString.matrixPriv[i])) * sizeof (*allD), sizeof (*allD));
-        allDigits(atoi(mString.matrixPriv[i]), allD);
-        //Guardar os valores de allD em mInts.matrixPriv[i][j-1]
-        for (int j = 1; j <= columnsPriv; ++j) {
-            //Se J ultrapassar o numero de digitos em mString.matrixPriv[i] guarda o valor -1 (este vai ser usardo como valor de referencia para uma coluna vazia visto que é ncessário escrever os '0')
-            if(j <= numDigits(atoi(mString.matrixPriv[i]))){
-                mInts.matrixPriv[i][j-1] = allD[numDigits(atoi(mString.matrixPriv[i]))-j];
-            }else{
-                mInts.matrixPriv[i][j-1] = -1;
-            }
-        }
-        free(allD);
-    }
-}
-
-void receiveMatrixCodInt(struct matrixString mString, struct matrixInts mInts, int lines){
-    for (int i = 0; i < lines-1; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            //Se mString.matrixPriv[i] for uma string vazia inserimos -1 (este vai ser usardo como valor de referencia para uma coluna vazia visto que é ncessário escrever os '0')
-            if(strcmp(mString.matrixPriv[i], "\0")){
-                mInts.matrixCod[i][j] = mString.matrixCod[i][j] - '0';
-            }else{
-                mInts.matrixCod[i][j] = -1;
-            }
-        }
-    }
-}
-
-struct matrixString removeKeyMatrix(struct matrixString mString, char *key, int lines){
-    for (int i = 0; i < lines; ++i) {
-        if(strcmp(mString.matrixPub[i], key) == 0){
-            for (int j = i; j < lines-1; ++j) {
-                strcpy(mString.matrixPub[j], mString.matrixPub[j+1]);
-                strcpy(mString.matrixPriv[j], mString.matrixPriv[j+1]);
-                strcpy(mString.matrixCod[j], mString.matrixCod[j+1]);
-            }
-            lines--;
-        }
-    }
-    return mString;
-}
-
 /**
  * Função que vai calcular o número de digitos de um número (int)
  * @param num número usado para calcular o seu número de digitos
@@ -704,6 +335,12 @@ short randomKeyValueShort(short r){
     return r;
 }
 
+/**
+ * Algoritmos de ordenacao ShellSort
+ * @param a mmatriz de strings
+ * @param N tamanho da matriz
+ * @param order ordem de ordenação (1 - ascendente / 0 - descendente)
+ */
 void shellSortChar(char** a, int N, int order){
     int h = 1;
     while (h < N/3){
@@ -727,6 +364,14 @@ void shellSortChar(char** a, int N, int order){
     }
 }
 
+/**
+ * Algoritmos de ordenacao ShellSort (ordenar por numero de digitos)
+ * @param a matriz de strings com chaves publicas
+ * @param priv matriz de strings com chaves privadas
+ * @param cod matriz de strings com chaves codificadas
+ * @param N tamanho das matrizes
+ * @param order ordem de ordenação (1 - ascendente / 0 - descendente)
+ */
 void shellSortCharDigits(char** a, char** priv, char** cod, int N, int order){
     int h = 1;
     while (h < N/3){
@@ -750,6 +395,14 @@ void shellSortCharDigits(char** a, char** priv, char** cod, int N, int order){
     }
 }
 
+/**
+ * Troca valores de 2 posições diferentes
+ * @param a matriz de strings com chaves publicas
+ * @param priv matriz de strings com chaves privadas
+ * @param cod matriz de strigns com chaves codificadas
+ * @param i primeira posição do array a ser trocada
+ * @param min segunda posição do array a ser trocada
+ */
 void swapCharDigits(char** a, char** priv, char** cod, int i, int min){
 
     char* temp = (char *) calloc(strlen(a[min]) * sizeof (char), sizeof (char));
@@ -766,6 +419,12 @@ void swapCharDigits(char** a, char** priv, char** cod, int i, int min){
     free(temp);
 }
 
+/**
+ * Troca valores de 2 posições diferentes
+ * @param a matriz de strings
+ * @param i primeira posição do array a ser trocada
+ * @param min segunda posição do array a ser trocada
+ */
 void swapChar(char** a, int i, int min){
 
     char* temp = (char *) calloc(strlen(a[min]) * sizeof (char), sizeof (char));
@@ -776,6 +435,12 @@ void swapChar(char** a, int i, int min){
     free(temp);
 }
 
+/**
+ * Algoritmos de ordenacao ShellSort
+ * @param a matriz de short
+ * @param N tamanho da matriz
+ * @param order ordem de ordenação (1 - ascendente / 0 - descendente)
+ */
 void shellSortInt(short** a, int N, int order){
     int h = 1;
     while (h < N/3){
@@ -799,6 +464,14 @@ void shellSortInt(short** a, int N, int order){
     }
 }
 
+ /**
+  * Algoritmos de ordenacao ShellSort (ordenar por numero de digitos)
+  * @param a matriz de short com chaves publicas
+  * @param priv matriz de short com chaves privadas
+  * @param cod matriz de short com chaves codificadas
+  * @param N tamanho das matrizes
+  * @param order ordem de ordenação (1 - ascendente / 0 - descendente)
+  */
 void shellSortIntDigits(short** a, short** priv, short** cod, int N, int order){
     int h = 1;
     while (h < N/3){
@@ -822,6 +495,14 @@ void shellSortIntDigits(short** a, short** priv, short** cod, int N, int order){
     }
 }
 
+/**
+ * Troca valores de 2 posições diferentes
+ * @param a matriz de short com chaves publicas
+ * @param priv matriz de short com chaves privadas
+ * @param cod matriz de short com chaves codificadas
+ * @param i primeira posição do array a ser trocada
+ * @param min segunda posição do array a ser trocada
+*/
 void swapIntDigits(short** a, short** priv, short** cod, int i, int min){
 
     short* temp = (short *) calloc(numDigitsLong(key_digits_2_long_int(a[min])), sizeof (short ));
